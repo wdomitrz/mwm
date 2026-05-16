1aa7be2d40def648ac625959ca9dc9df885e9822
Witalis Domitrz <witekdomitrz@gmail.com>
2026-05-16 13:52:54 +0100

    Use uv

--- mwm.py
+++ mwm.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S uv run --script
 ################################################################
 # Copyright (c) 2026 Witalis Domitrz <witekdomitrz@gmail.com>
 # AGPL License
